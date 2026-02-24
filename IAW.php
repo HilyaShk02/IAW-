473fa52c1c6518f9d2f2e4e278b781b11cc15f7c
HilyaShk02 <hilyanshkmarts@gmail.com>
2026-02-24 08:22:00 +0000

    Primer commit del archivo IAW.php

---
+++ IAW.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>¡Hola Mundo PHP!</title>
+</head>
+<body style="font-family: Arial; padding: 50px;">
+    
+    <h1> ¡Hola !</h1>
+    
+    <!-- FORMULARIO SIMPLE -->
+    <form method="POST">
+        <p>Tu nombre: 
+            <input type="text" name="nombre" style="padding: 10px; font-size: 16px;">
+            <input type="submit" value="¡Enviar!" style="padding: 10px 20px; background: #4CAF50; color: white; border: none;">
+        </p>
+    </form>
+    
+    <!-- PHP AQUÍ -->
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nombre = $_POST["nombre"];
+            echo "<h2>Hola, $nombre 👋</h2>";
+        }
+    ?>
+    
+</body>
+</html>
