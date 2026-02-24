a674e779c10bd524e853ced7331cc13a029cc024
HilyaShk02 <hilyanshkmarts@gmail.com>
2026-02-24 08:58:21 +0000

    Actividad 2: Tratamiento de datos del alumno

---
+++ datos_alumno.php
@@ -0,0 +1,11 @@
+<?php
+// Declarar variables
+$nombre = "Ana";      // Pon tu nombre aquí
+$ciclo = "ASIR";      // Nombre del ciclo
+$curso = "2º";        // Curso
+
+// Mostrar la frase
+echo "Me llamo $nombre, estudio $ciclo y estoy en $curso curso.";
+?>
+
+
